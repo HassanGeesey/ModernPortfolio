23ac8d8a174f008c6cb0460b6debda0f9d9e51dd
HassanGeesey <xasangeesey82@gmail.com>
2025-12-28 09:46:10 +0300

    the image was not showing so i deployed it , by adding a new folder called assets image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -61,7 +61,7 @@ export const Hero: React.FC = () => {
             <div className="relative glass p-4 rounded-[4rem] transform hover:rotate-2 transition-transform duration-700">
               <div className="rounded-[3rem] overflow-hidden aspect-[3/4] border border-white/10 shadow-2xl">
                 <img
-                  src="components/src/myimage.jpeg"
+                  src="assets\myimage.jpeg"
                   alt="Portrait"
                   className="w-full h-full object-cover saturate-0 hover:saturate-100 transition-all duration-1000"
                 />
